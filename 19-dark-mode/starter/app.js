0750d99fda958f63c046540e21e9d0a5ae70b1a4
Maria Shishmanova <shishmanova.mariya@gmail.com>
2024-04-30 15:09:22 +0300

    dark mode finished

--- 19-dark-mode/starter/app.js
+++ 19-dark-mode/starter/app.js
@@ -1,1 +1,27 @@
-console.log('dark mode');
+import articles from './data.js';
+
+const toggleBtn = document.querySelector('.btn');
+const container = document.querySelector('.articles');
+
+toggleBtn.addEventListener('click', () => {
+  document.documentElement.classList.toggle('dark-theme');
+});
+
+const newArticles = articles
+  .map(({title, date, length, snippet}) => {
+    // format date
+    const formatDate =  moment(date).format('MMMM Do, YYYY')
+    return `<article class="post">
+  <h2>${title}</h2>
+  <div class="post-info">
+    <span>${formatDate}</span>
+    <span>${length} min read</span>
+  </div>
+
+  <p>${snippet}</p>
+</article>`;
+  })
+  .join('');
+
+container.innerHTML = newArticles;
+
