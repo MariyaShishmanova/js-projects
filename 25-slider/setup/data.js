d5faedd5fbb9a600464bfd9177b1bd3de5960350
Maria Shishmanova <shishmanova.mariya@gmail.com>
2024-05-02 18:09:05 +0300

    slider finished

--- 25-slider/setup/data.js
+++ 25-slider/setup/data.js
@@ -16,7 +16,7 @@ const people = [
     name: 'emma doe',
     job: 'designer',
     text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
-  },
+  }
 ];
 
 export default people;
