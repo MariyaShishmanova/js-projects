a80a4c7e783fb0b455d1e06603d64ade20ae76a2
Maria Shishmanova <shishmanova.mariya@gmail.com>
2024-05-03 14:42:40 +0300

    pagination finished

--- 27-pagination/setup/paginate.js
+++ 27-pagination/setup/paginate.js
@@ -1,3 +1,15 @@
-const paginate = () => {}
+const paginate = followers => {
+  const itemsPerPage = 9;
+  const numberOfPages = Math.ceil(followers.length / itemsPerPage);
 
-export default paginate
+  const newFollowers = Array.from({ length: numberOfPages }, (_, index) => {
+    // start represents the index from which to start selecting followers for the current page.
+    const start = index * itemsPerPage;
+
+    // slice select a portion of the followers array starting from the start index and ending at start + itemsPerPage
+    return followers.slice(start, start + itemsPerPage);
+  });
+  return newFollowers;
+};
+
+export default paginate;
