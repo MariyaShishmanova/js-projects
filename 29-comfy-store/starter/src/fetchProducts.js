67b6cd53416070029a3c6feed7ced7b1f84799c9
Maria Shishmanova <shishmanova.mariya@gmail.com>
2024-05-09 12:29:32 +0300

    fetch products, set local storage, display products functionalities added

--- 29-comfy-store/starter/src/fetchProducts.js
+++ 29-comfy-store/starter/src/fetchProducts.js
@@ -1,5 +1,15 @@
 import { allProductsUrl } from './utils.js';
 
-const fetchProducts = async () => {};
+const fetchProducts = async () => {
+  try {
+    const response = await fetch(allProductsUrl);
+    if (response) {
+      return response.json();
+    }
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export default fetchProducts;
